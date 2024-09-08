7f103cf60649520416bd85bbe8e7404b881971b3
sonyokpara <sony.okpara@yahoo.com>
2024-09-08 09:25:24 +0000

    custom events

--- app.js
+++ app.js
@@ -1,42 +1,11 @@
-const { readFile, writeFile } = require("fs").promises;
-// const util = require("util");
+const EventEmitter = require("events");
+const customeEvent = new EventEmitter();
 
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+customeEvent.on("response", () => {
+  console.log("response recieved!");
+});
+customeEvent.on("response", () => {
+  console.log("response is processing...");
+});
 
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, "utf8", (err, result) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(result);
-//       }
-//     });
-//   });
-// };
-
-// getText("./content/first.txt")
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-const start = async () => {
-  try {
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
-    await writeFile(
-      "./content/result-async.txt",
-      `THIS IS AWESOME: ${first}, ${second}`,
-      { flag: "a" }
-    );
-    console.log(first, second);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
+customeEvent.emit("response");
